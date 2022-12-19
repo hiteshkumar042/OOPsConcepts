c698420f5611ad301f0d344e82b7dbe7329691a6
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 08:48:13 +0530

    Added Inheritance

---
+++ OOPs Concepts/Inheritance/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concepts.Inheritance
+{
+    internal class Vehicle            // Parent Class
+    {
+        public string brand = "Kia";  // Vehicle Brand
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle  // Derived Class (Child Class)
+    {
+        public string modelName = "Seltos";  // Car field
+    }
+}
--- OOPs Concepts/Program.cs
+++ OOPs Concepts/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace OOPs_Concepts
+﻿using OOPs_Concepts.Inheritance;
+
+namespace OOPs_Concepts
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to OOPs Concepts");
+            Console.WriteLine("Welcome to OOPs Concepts!");
+            Console.WriteLine("Choose Option : \n1. Inheritance");
+            int option =Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1: // Create a myCar object
+                    Car myCar = new Car();
+
+                    // Call the honk() method (From the Vehicle class) on the myCar object
+                    myCar.honk();
+
+                    // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+                    Console.WriteLine(myCar.brand);
+                    Console.WriteLine(myCar.modelName);
+                break;
+
+                default:
+                    Console.WriteLine("Choose from given Option only");
+                break;
+            }
+
+            
         }
     }
 }
