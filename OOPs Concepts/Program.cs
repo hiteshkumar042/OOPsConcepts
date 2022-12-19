c884917eb4236242168ade936663953f22c43db7
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 08:33:32 +0530

    Welcome to OOPs Concepts

---
+++ OOPs Concepts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPs_Concepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to OOPs Concepts");
+        }
+    }
+}
