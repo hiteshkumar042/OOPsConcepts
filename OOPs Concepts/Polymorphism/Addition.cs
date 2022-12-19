79c4997f2215e5abec814378155ff84c83bb81a6
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 10:40:10 +0530

    Pplymorphism concepts uploaded

---
+++ OOPs Concepts/Polymorphism/Addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs_Concepts.Polymorphism
+{
+    internal class Addition
+    {
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public float Add(int firstNum, float secondNum, long thirdNum)
+        {
+            float result = firstNum + secondNum + thirdNum;
+            return result;
+        }
+        public float Add(float firstNum, float secondNum, float thirdNum)
+        {
+            float result = firstNum + secondNum + thirdNum;
+            return result;
+        }
+    }
+}
--- OOPs Concepts/Program.cs
+++ OOPs Concepts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OOPs_Concepts.Abstraction;
 using OOPs_Concepts.Inheritance;
+using OOPs_Concepts.Polymorphism;
 
 namespace OOPs_Concepts
 {
@@ -8,31 +9,45 @@ namespace OOPs_Concepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to OOPs Concepts!");
-            Console.WriteLine("Choose Option : \n1. Inheritance \n2. Abstraction");
+            Console.WriteLine("Choose Option : \n1. Inheritance \n2. Abstraction \n3.Polymorphism - Method Ovreriding \n4. Polymorphism - Method Overloading");
             int option =Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
-                    case 1: //Inheritance
-                        // Create a myCar object
-                        Car myCar = new Car();
+               case 1: //Inheritance
+                      // Create a myCar object
+                      Car myCar = new Car();
 
-                        // Call the honk() method (From the Vehicle class) on the myCar object
-                        myCar.honk();
+                      // Call the honk() method (From the Vehicle class) on the myCar object
+                      myCar.honk();
 
-                        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
-                        Console.WriteLine(myCar.brand);
-                        Console.WriteLine(myCar.modelName);
-                    break;
+                      // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+                      Console.WriteLine(myCar.brand);
+                      Console.WriteLine(myCar.modelName);
+                break;
                 
-                    case 2: //Abstract
-                         Pig myPig = new Pig(); // Create a Pig object
-                         myPig.animalSound();  // Call the abstract method
-                         myPig.sleep();  // Call the regular method
+                case 2: //Abstract
+                      Pig myPig = new Pig(); // Create a Pig object
+                      myPig.animalSound();  // Call the abstract method
+                      myPig.sleep();  // Call the regular method
+                break;
+         
+                case 3: //Polymorphism - Method Ovreriding
+                    Animal1 animal1 = new Animal1();
+                    animal1.Run();
+                    Dog dog = new Dog();
+                    dog.Run();
+                    break;
+                    
+                case 4: //4.Polymorphism - Method Overloading
+                    Addition addition = new Addition();
+                    Console.WriteLine("Sum: " + addition.Add(2, 3));
+                    Console.WriteLine("Sum: " + addition.Add(3, 5, 6));
+                    Console.WriteLine("Sum: " + addition.Add(9, 3.12f, 32));
                     break;
 
-                    default:
-                        Console.WriteLine("Choose from given Option only");
-                    break;
+                default:
+                     Console.WriteLine("Choose from given Option only");
+                break;
             }
 
             
